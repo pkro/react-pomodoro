f391766bcf5a18da6c178b3720143055bfdba963
pkro <pkrombach@web.de>
2020-02-15 10:05:11 +0100

    default idas default args

--- src/ToggleButton.jsx
+++ src/ToggleButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function ToggleButton({ onClick, buttonText, id }) {
+export default function ToggleButton({ onClick, buttonText, id = 'toggleButton' }) {
   return (
-    <button className="toggleButton" type="button" onClick={onClick} id={id || " toggleButton"}>
+    <button className="toggleButton" type="button" onClick={onClick} id={id}>
       {buttonText}
     </button >
   );
