29258bfb0808711705f802b6654e47e74c2285bd
pkro <pkrombach@web.de>
2020-02-15 10:17:13 +0100

    pointless button module removed

--- src/ToggleButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ToggleButton({ onClick, buttonText, id = 'toggleButton' }) {
-  return (
-    <button className="toggleButton" type="button" onClick={onClick} id={id}>
-      {buttonText}
-    </button >
-  );
-}
