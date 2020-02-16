854056f7b794309ad81b1693df11cf5919b5ed77
pkro <pkrombach@web.de>
2020-02-16 17:36:39 +0100

    material ui'd activity log

--- src/Activitys/ActivityLog.jsx
+++ src/Activitys/ActivityLog.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import TimeDisplay from '../TimeDisplay';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 export default function activityLog({ log }) {
   return (
     <div className="activityLog">
-      <table>
-        <thead>
-          <tr>
-            <td>ID</td>
-            <td>Time spent</td>
-          </tr>
-        </thead>
-        <tbody>
-          {log.activities.map(obj => {
-            return (
-              <tr key={obj.key}>
-                <td>{obj.id}</td>
-                <td>
-                  <TimeDisplay seconds={obj.timeSpent} />
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <TableContainer component={Paper}>
+        <Table size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Time spent</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {log.activities.map(obj => {
+              return (
+                <TableRow key={obj.key}>
+                  <TableCell>{obj.id}</TableCell>
+                  <TableCell>
+                    <TimeDisplay seconds={obj.timeSpent} />
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
