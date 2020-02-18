2c71160d9c75422cfbbd575b194152af112972cd
pkro <pkrombach@web.de>
2020-02-18 16:54:48 +0100

    moved seconds2display out of component for reuse

--- src/TimeDisplay.jsx
+++ src/TimeDisplay.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function TimeDisplay({ seconds, id }) {
-  function seconds2display(numSeconds) {
-    const minutes = Math.floor(numSeconds / 60).toString().padStart(2, '0');
-    const newSeconds = (numSeconds % 60).toString().padStart(2, '0');
-    return `${minutes}:${newSeconds}`;
-  }
+export function seconds2display(numSeconds) {
+  const minutes = Math.floor(numSeconds / 60).toString().padStart(2, '0');
+  const newSeconds = (numSeconds % 60).toString().padStart(2, '0');
+  return `${minutes}:${newSeconds}`;
+}
 
+export default function TimeDisplay({ seconds, id }) {
   const displayTime = seconds2display(seconds);
 
   if (id === undefined) id = 'Minutes';
