b1d18b87d98ea6939491793a0ff2736f89e301f0
pkro <pkrombach@web.de>
2020-03-01 16:22:29 +0100

    Updates

---
+++ precache-manifest.069a27c1e73485be2d46fe2c79332080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4e1dfd89f315c14e9356d97a55f616",
+    "url": "/react-pomodoro/index.html"
+  },
+  {
+    "revision": "e42abc31371eb68641fb",
+    "url": "/react-pomodoro/static/css/main.e88e3fb5.chunk.css"
+  },
+  {
+    "revision": "77178127f8294308b3a6",
+    "url": "/react-pomodoro/static/js/2.77d9730f.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/react-pomodoro/static/js/2.77d9730f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e42abc31371eb68641fb",
+    "url": "/react-pomodoro/static/js/main.f71e41dc.chunk.js"
+  },
+  {
+    "revision": "6eaeeeacc916ec18fcd4",
+    "url": "/react-pomodoro/static/js/runtime-main.bf17695f.js"
+  }
+]);
